8856fe14d82632184e8f8bb6ff7dbc03ef69ecc2
Vitor Cioletti <ciolettiv@gmail.com>
2018-11-11 21:43:05 -0200

    Retirado projeto webapi core para net framework

--- Web-API/Startup.cs
+++ Web-API/Startup.cs
@@ -3,18 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-namespace WebAPI
-{
-	using Microsoft.AspNetCore.Hosting;
-	using Microsoft.AspNetCore.HttpsPolicy;
-	using Microsoft.AspNetCore.Mvc;
-	using Microsoft.Extensions.Configuration;
-	using Microsoft.Extensions.DependencyInjection;
-	using Microsoft.Extensions.Logging;
-	using Microsoft.Extensions.Options;
-	using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
-	public class Startup
+namespace Web_API
+{
+    public class Startup
     {
         public Startup(IConfiguration configuration)
         {
@@ -26,7 +23,7 @@ namespace WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,12 +33,7 @@ namespace WebAPI
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseHsts();
-            }
 
-            app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
