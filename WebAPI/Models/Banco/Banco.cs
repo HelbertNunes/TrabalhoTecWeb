11053d56bc77abf0801500d4eaf82421d9754c04
Vitor Cioletti <ciolettiv@gmail.com>
2018-11-11 23:03:51 -0200

    Adicionado projeto WEBAI

---
+++ WebAPI/Models/Banco/Banco.cs
@@ -0,0 +1,28 @@
+﻿namespace WebAPI.Models.Banco
+{
+	using QueryLibrary;
+	using System.Collections.Generic;
+
+	public static class Banco
+    {
+		private static Query _query = new Query("Server=tcp:fitlifeserver.database.windows.net,1433;Initial Catalog=FitLifeDB;Persist Security Info=False;User ID={USUARIO};Password={SENHA};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+
+		public static Alimento ObtemAlimento(Macronutrientes macronutrientes)
+		{
+			return _query.SelectExactlyOne<Alimento>(@"SELECT * FROM Alimento 
+				WHERE (Gordura BETWEEN @Gordura AND @Gordura + 50) AND (Proteina BETWEEN @Proteina AND @Proteina + 50)
+				AND (Carboidrato BETWEEN @Carboidrato AND @Carboidrato + 50)", macronutrientes);
+		}
+
+		public static IEnumerable<Dieta> ObtemDietas(string idUsuario) => new List<Dieta>();
+
+		public static Dieta RemoveDieta(string idDieta) => new Dieta();
+
+		public static Usuario ObtemUsuario(string emailUsuario) => new Usuario();
+
+		public static Usuario EditaUsuario(Usuario usuario) => usuario;
+
+		public static Usuario RemoveUsuario(Usuario usuario) => usuario;
+
+    }
+}
---
+++ WebAPI/Models/Dieta.cs
@@ -0,0 +1,15 @@
+﻿namespace WebAPI.Models
+{
+	using System.Collections.Generic;
+
+	public class Dieta
+    {
+		public string Id { get; set; }
+
+		public Usuario Usuario { get; set; }
+
+		public IEnumerable<Alimento> Alimentos { get; set; }
+
+		public string Nome { get; set; }
+	}
+}
