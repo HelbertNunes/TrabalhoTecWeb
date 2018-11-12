1e96cf4c0c648b3f593b00dba28e0fcd856860c4
Vitor Cioletti <ciolettiv@gmail.com>
2018-11-11 23:29:12 -0200

    Adicionado arquivo de AJAX

---
+++ Inspinia_MVC5_SeedProject/Scripts/requisicao.js
@@ -0,0 +1,41 @@
+﻿function get(rota, dado, sucesso, falha) {
+    var _url = rota;
+
+    $.ajax({
+        url: _url,
+        type: 'GET',
+        data: JSON.stringify(dado),
+        contentType: "application/json",
+        dataType: 'json',
+        success : sucesso,
+        error : (jqXHR, _) => falha(jqXHR.responseText)
+    })
+}
+
+function post(rota, dado, sucesso, falha) {
+    var _url = rota;
+
+    $.ajax({
+        url: _url,
+        type: 'POST',
+        contentType: "application/json",
+        dataType: 'json',
+        data: JSON.stringify(dado),
+        success : sucesso,
+        error : (jqXHR, _) => falha(jqXHR.responseText)
+    })
+}
+
+function del(rota, dado, sucesso, falha) {
+    var _url = rota;
+
+    $.ajax({
+        url: _url,
+        type: 'DELETE',
+        data: JSON.stringify(dado),
+        contentType: "application/json",
+        dataType: 'json',
+        success : sucesso,
+        error : (jqXHR, _) => falha(jqXHR.responseText)
+    })
+}
