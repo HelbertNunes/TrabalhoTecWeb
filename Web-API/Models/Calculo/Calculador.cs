5270a04d92ec6c931ae6cbbee78f272b506bdfc1
Vitor Cioletti <ciolettiv@gmail.com>
2018-11-11 23:03:38 -0200

    Adicionado projeto WebAPI

--- Web-API/Models/Calculo/Calculador.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebAPI.Models.Calculo
-{
-	using Banco;
-
-    public static class Calculador
-    {
-		public static Dieta GeraDieta(Macronutrientes macronutrientes) => new Dieta();
-    }
-}
--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -1,18 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-namespace WebAPI
+﻿namespace WebAPI
 {
+	using Microsoft.AspNetCore.Builder;
 	using Microsoft.AspNetCore.Hosting;
-	using Microsoft.AspNetCore.HttpsPolicy;
-	using Microsoft.AspNetCore.Mvc;
 	using Microsoft.Extensions.Configuration;
 	using Microsoft.Extensions.DependencyInjection;
-	using Microsoft.Extensions.Logging;
-	using Microsoft.Extensions.Options;
-	using Microsoft.EntityFrameworkCore;
 
 	public class Startup
     {
@@ -23,25 +14,13 @@ namespace WebAPI
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-        }
+        public void ConfigureServices(IServiceCollection services) => services.AddMvc();
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
-            {
                 app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseHsts();
-            }
 
-            app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
